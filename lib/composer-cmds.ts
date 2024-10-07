0d64ae5ee6f5fb48b12732ac63b64f9469dc3569
Damilola Olufemi <dami.olufemi@snyk.io>
2024-10-07 14:28:29 +0100

    feat: replace execSync with spawnSync

--- lib/composer-cmds.ts
+++ lib/composer-cmds.ts
@@ -6,10 +6,10 @@ export const composerShowCmd = 'composer show -p';
 export const pharCmd = `php ${path.resolve(path.resolve() + '/composer.phar')} show -p --format=json`;
 
 export function cmdReturnsOk(cmd): boolean {
-  return cmd && childProcess.spawnSync(cmd, { shell: true }).status === 0;
+  return cmd && childProcess.spawnSync(cmd, { shell: false }).status === 0;
 }
 
 // run a cmd in a specific folder and it's result should be there
 export function execWithResult(cmd, basePath): string {
-  return childProcess.execSync(cmd, { cwd: basePath }).toString();
+  return childProcess.spawnSync(cmd, { cwd: basePath, shell: false }).toString();
 }
