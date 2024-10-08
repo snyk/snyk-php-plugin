9189f093b94f9ce51672f6919ffbc98171fd66d4
Damilola Olufemi <dami.olufemi@snyk.io>
2024-10-08 09:29:32 +0100

    feat: pass command and args into spawn separately

--- lib/system-deps.ts
+++ lib/system-deps.ts
@@ -9,9 +9,9 @@ function isSet(variable): boolean {
 }
 
 export function systemDeps(basePath: string, options: PhpOptions): SystemPackages {
-  const composerOk = isSet(options.composerIsFine) ? options.composerIsFine : cmds.cmdReturnsOk(cmds.composerCmd);
+  const composerOk = isSet(options.composerIsFine) ? options.composerIsFine : cmds.cmdReturnsOk(cmds.composerCmd.command,cmds.composerCmd.args);
   const composerPharOk = isSet(options.composerPharIsFine) ?
-    options.composerPharIsFine : cmds.cmdReturnsOk(cmds.pharCmd);
+    options.composerPharIsFine : cmds.cmdReturnsOk(cmds.pharCmd.command, cmds.pharCmd.args);
 
   let finalVersionsObj = {};
 
@@ -19,7 +19,7 @@ export function systemDeps(basePath: string, options: PhpOptions): SystemPackage
     // give first preference to a stub
     finalVersionsObj = options.systemVersions;
   } else if (composerOk) {
-    const lines = cmds.execWithResult(cmds.composerShowCmd, basePath).split(os.EOL);
+    const lines = cmds.execWithResult(cmds.composerShowCmd.command, basePath, cmds.composerShowCmd.args).split(os.EOL);
     lines.forEach((line) => {
       const [part1, part2] = line.split(/\s+/);
       if (part2) {
@@ -27,7 +27,7 @@ export function systemDeps(basePath: string, options: PhpOptions): SystemPackage
       }
     });
   } else if (composerPharOk) {
-    const output = cmds.execWithResult(cmds.pharCmd, basePath);
+    const output = cmds.execWithResult(cmds.pharCmd.command, basePath, cmds.pharCmd.args);
     const versionsObj = JSON.parse(output).platform;
     versionsObj.forEach(({name, version}) => {
       finalVersionsObj[name] = version;
