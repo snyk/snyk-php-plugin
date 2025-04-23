3bf98b88c547ce1ef2db3e9b171243f3756aa8ac
Nicolas Séverin <nicolas.severin@snyk.io>
2025-04-23 17:17:18 +0100

    fix(OSM-2584): use spawnSync result stdout on success or throw an error

--- lib/composer-cmds.ts
+++ lib/composer-cmds.ts
@@ -1,17 +1,27 @@
 import * as path from 'path';
 import * as childProcess from 'child_process';
-export const composerCmd = {command: 'composer', args: ['--version']};
+
+export const composerVersionCmd = {command: 'composer', args: ['--version']};
 export const composerShowCmd = {command: 'composer', args: ['show', '-p']};
-export const pharCmd = {command: `php`, args:[`${path.resolve(path.resolve() + '/composer.phar')}`, 'show', '-p', '--format=json']
+export const pharVersionCmd = {
+  command: `php`,
+  args: [`${path.resolve(path.resolve() + '/composer.phar')}`, '--version']
+};
+export const pharShowCmd = {
+  command: `php`, args: [`${path.resolve(path.resolve() + '/composer.phar')}`, 'show', '-p', '--format=json']
 };
 
 export function cmdReturnsOk(cmd, args: string[] = []): boolean {
   const spawnOptions: childProcess.SpawnOptions = { shell: false };
-  return cmd && childProcess.spawnSync(cmd, args,spawnOptions).status === 0;
+  return cmd && childProcess.spawnSync(cmd, args, spawnOptions).status === 0;
 }
 
 // run a cmd in a specific folder and it's result should be there
 export function execWithResult(cmd, basePath, args: string[] = []): string {
-  const spawnOptions: childProcess.SpawnOptions ={ cwd: basePath, shell: false }
-  return childProcess.spawnSync(cmd, args, spawnOptions).toString();
+  const spawnOptions: childProcess.SpawnOptions = { cwd: basePath, shell: false }
+  const execResult = childProcess.spawnSync(cmd, args, spawnOptions);
+  if (execResult.status !== 0) {
+    throw execResult;
+  }
+  return execResult.stdout.toString();
 }
